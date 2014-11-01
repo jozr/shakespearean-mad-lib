bc0369b81bc89ff4432da4eda51a0aa7507c224c
jozwright <jozwright@gmail.com>
2014-11-01 16:35:16 -0700

    add age restriction

--- Story.java
+++ Story.java
@@ -4,12 +4,14 @@ public class Story {
     
     public static void main(String[] args) {
         Console console = System.console();
-        /*  Some terms:
-            noun - Person, place or thing
-            verb - An action
-            adjective - A description used to modify or describe a noun
-            Enter your amazing code here!
-        */
+        String ageAsString = console.readLine("How old are you?  ");
+        int age = Integer.parseInt(ageAsString);
+        if (age < 13) {
+            //Insert exit code
+            console.printf("Sorry you must be 13 or older to use this program.\n");
+            System.exit(0);
+        }
+
         String noun = console.readLine("Noun:  ");
         String location = console.readLine("Specific location:  ");
         String bodyPart = console.readLine("Body part:  ");
@@ -20,8 +22,9 @@ public class Story {
         String nameThree = console.readLine("Another name:  ");
         String pluralNoun = console.readLine("Plural noun:  ");
         String relative = console.readLine("Relative i.e., 'sister':  ");
-        console.printf("~~~~~~~~~~~Shakespearean Prose~~~~~~~~~~~\n");
-        console.printf("Give me my %s, put on my crown; I have\nImmortal longings in me: now no more\nThe juice of %s's grape shall moist this %s:\nYare, yare, %s %s; quick. Methinks I hear\n%s call; I see him rouse himself\nTo praise my noble act; I hear him %s\nThe luck of %s, which the gods give %s\nTo excuse their after wrath: %s, I come:\n", noun, location, bodyPart, adjective, name, nameTwo, verb, nameThree, pluralNoun, relative);
+
+        console.printf("~~~~~~~~~~~~~Shakespearean Prose~~~~~~~~~~~~~\n\n");
+        console.printf("Give me my %s, put on my crown; I have\nImmortal longings in me: now no more\nThe juice of %s's grape shall moist this %s:\nYare, yare, %s %s; quick. Methinks I hear\n%s call; I see him rouse himself\nTo praise my noble act; I hear him %s\nThe luck of %s, which the gods give %s\nTo excuse their after wrath: %s, I come:\n\n", noun, location, bodyPart, adjective, name, nameTwo, verb, nameThree, pluralNoun, relative);
 		    }
 		    
 		}
